f451080d3f37dfc81164f13e91289085477f4189
William Bettridge-Radford <william.bettridge.radford@gail.com>
2021-11-29 23:10:33 +0000

    refactor to use a stack

--- src/parse.c
+++ src/parse.c
@@ -27,46 +27,35 @@
         exit(1); \
     } while (0);
 
+#define debug_var(s,v) \
+    fprintf(stderr, #v ": %" s "\n", v)
+
 typedef unsigned int uint;
-
-
-struct string {
-    uint size;
-    uint capacity;
-    char *cstr;
-};
-typedef struct string string;
-
-struct parser {
-    void (*chomp)();
-    void (*parse_token)();
-};
-typedef struct parser parser;
 
 uint cur_indent = 0;
 uint indent_width = 4;
-char break_buffer[256] = "";
 char line_buffer[256] = "";
-char token_buffer[256] = "";
-char *c = NULL;
+char tokens_buffer[256] = "";
+char *in = NULL;
 FILE *f = NULL;
-uint i = 0;
 char *tok = NULL;
 uint tok_len = 0;
 
-string token       = {};
-string line        = {};
-string break_chars = {0, sizeof(break_buffer) - 1, break_buffer};
+char *prefix_breakchars = " ,()[]{}\n";
 
-parser pp = {};
-parser *p = &pp;
+char *cmds[16];
+uint depth = 0;
+void (*(state_fn[16]))(void);
 
+
+void parse_prefix_body(void);
+void parse_prefix_head(void); 
 
 void
-chomp(void)
+chomp(char c)
 {
-    while (*c == ' ')
-        c += 1;
+    while (*in == c)
+        in += 1;
 }
 
 
@@ -76,160 +65,188 @@ read_line(void)
     char *r = fgets(line_buffer, 256, f);
     if (r == NULL)
         line_buffer[0] = '\0';
-    c = &line_buffer[0];
+    in = &line_buffer[0];
     return r;
 }
 
 
-int
-str_contains(string *s, char c)
+void
+read_token(void)
 {
-    char *i = s->cstr;
+    tok = in;
+    while (!strchr(prefix_breakchars, *in))
+        in += 1;
+    tok_len = in - tok;
+}
 
-    while (*i != '\0') {
-        if (*i == c)
-            return 1;
-        i += 1;
-    }
 
+int
+parse_indent(int *indent)
+{
+    char *first_char = NULL;
+    uint diff = 0;
+
+    while(*in == '\n')
+        if (read_line() == NULL)
+            return -1;
+
+    first_char = in;
+    chomp(' ');
+
+    diff = in - first_char;
+    ere;
+    debug_var("d", diff);
+    debug_var("d", in);
+    debug_var("d", first_char);
+    if (diff % indent_width != 0)
+        die("invalid indent");
+
+    *indent = diff / indent_width;
+    /*fprintf(stderr, "indent: %d\n", indent);*/
     return 0;
 }
 
 
 void
-parse_token(char *buf, uint *i)
+parse_prefix_body(void)
 {
-    tok = c;
-    while (!str_contains(&break_chars, *c))
-        c += 1;
-    tok_len = c - tok;
+    if (*in == '\0')
+        die("null char");
+
+
+    if (*in == '\n') {
+        int new_indent = 0;
+        int diff       = 0;
+
+        if (parse_indent(&new_indent)) {
+            tok = cmds[depth];
+            tok_len = strlen(tok);
+            state_fn[depth] = parse_prefix_head;
+            return;
+        }
+
+        ere;
+        debug_var("d", cur_indent);
+        debug_var("d", new_indent);
+
+        diff = new_indent - cur_indent;
+        cur_indent = new_indent;
+        debug_var("d", diff);
+
+        die("newline")
+    }
+
+    if (*in == ' ')
+        die("space")
+
+    read_token();
+    chomp(' ');
+
+
+
+
+    /*printf("tok: %.*s\n", tok_len, tok);*/
+    /*die("ere");;*/
 }
-
-
-int
-parse_newline(parser *p)
-{
-    char *first_char = NULL;
-    uint indent = 0;
-    uint diff = 0;
-
-    while(*c == '\n')
-        if (read_line() == NULL)
-            return -1;
-
-    first_char = c;
-    chomp();
-
-    diff = c - first_char;
-    if (diff % indent_width != 0)
-        die("invalid indent");
-
-    indent = diff / indent_width;
-    /*fprintf(stderr, "indent: %d\n", indent);*/
-    return indent;
-}
-
 
 void
-next_word(parser *p)
+parse_prefix_head(void)
 {
-    uint new_indent = 0;
-    uint diff       = 0;
-
-    p->chomp();
-
-    if (*c == '\0') {
-        tok = NULL;
-        return;
-    } else if (*c == '\n') {
-        new_indent = parse_newline(p);
-        if (new_indent == -1) {
-            /*ere;*/
+    if (*in == '\0') {
+        if (read_line() == NULL) {
             tok = NULL;
             return;
         }
-        diff = new_indent - cur_indent;
-
-        p->parse_token();
-
-        if (tok_len == 0)
-            return;
-
-        if (!strncmp("\\", tok, tok_len)) {
-            if (diff == 1) {
-                c += 1;
-                p->chomp();
-                p->parse_token();
-            } else {
-                die("line continuation2");
-            }
-        } else {
-            printf("c: %d\n", *c);
-            printf("tok '%.*s'\n", tok_len, tok);
-            die("newline");
-        }
-
-        return;
     }
 
-    p->parse_token();
-}
+    if (*in == '\0')
+        die("null char")
 
+    if (*in == '\n')
+        die("newline")
 
-int
-cstr_len(char *s)
-{
-    int i = 0;
+    if (*in == ' ')
+        die("space")
 
-    if (s == NULL)
-        die("s is NULL");
+    read_token();
+    /*printf("%.*s\n", tok_len, tok);*/
 
-    while (*s++ != '\0')
-        i += 1;
-    return i;
-}
+    char *cmd = malloc(tok_len + 1);
+    if (cmd == NULL)
+        die("malloc failed");
+    strncpy(cmd, tok, tok_len);
+    /*debug_var("s", cmd);*/
+    cmds[depth] = cmd;
 
+    chomp(' ');
 
-void
-str_append_chars(string *s, char *cs)
-{
-    char *i = cs;
-    char *d = s->cstr + s->size;
-    int cs_len = cstr_len(cs);
-    uint new_size = s->size + cs_len;
+    state_fn[depth] = parse_prefix_body;
+    parse_prefix_body();
 
-    if (new_size > s->capacity)
-        die("capacity reached");
+    /*if (*in == '\0') {*/
+        /*tok = NULL;*/
+        /*return;*/
+    /*} else if (*in == '\n') {*/
+        /*new_indent = parse_indent();*/
+        /*if (new_indent == -1) {*/
+            /*[>ere;<]*/
+            /*tok = NULL;*/
+            /*return;*/
+        /*}*/
 
-    while (*i != '\0')
-        *d++ = *i++;
-    *d = '\0';
+        /*diff = new_indent - cur_indent;*/
+        /*cur_indent = new_indent;*/
+        /*debug_var("d", diff);*/
 
-    s->size = new_size;
-}
+        /*parse_token();*/
 
-void
-init(void)
-{
-    p->chomp = chomp;
-    p->parse_token = parse_token;
+        /*if (tok_len == 0)*/
+            /*return;*/
 
-    str_append_chars(&break_chars, " ,()[]{}\n");
+        /*if (!strncmp("\\", tok, tok_len)) {*/
+            /*if (diff == 1) {*/
+                /*in += 1;*/
+                /*chomp(' ');*/
+                /*parse_token();*/
+            /*} else {*/
+                /*die("line continuation2");*/
+            /*}*/
+        /*} else {*/
+            /*[>debug_var("d", diff);<]*/
+            /*[>debug_var("d", new_indent);<]*/
+            /*[>debug_var("d", *c);<]*/
+            /*[>printf("tok '%.*s'\n", tok_len, tok);<]*/
+            /*[>die("newline");<]*/
+        /*}*/
+
+        /*return;*/
+    /*}*/
+
+    /*parse_token();*/
+    /*chomp(' ');*/
 }
 
 
 int
 main(int argc, char **argv)
 {
-    init();
-
-    if ((f = fopen(".\\src\\examples\\tokens1.ie", "r")) == NULL)
+    if ((f = fopen(".\\src\\examples\\tokens0.ie", "r")) == NULL)
         die("failed to open file");
 
     read_line();
 
-    while (next_word(p), tok) {
+    depth = 0;
+    state_fn[depth] = parse_prefix_head;
+
+    while (state_fn[depth](), tok) {
         printf("tok: %d: '%.*s'\n", tok_len, tok_len, tok);
+    }
+
+    /*debug_var("d", depth);*/
+
+    while (depth) {
+        printf("%s\n", cmds[depth]);
+        depth -= 1;
     }
 
     fclose(f);
