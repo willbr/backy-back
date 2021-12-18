a4e1cac01aac310dbe9a7880cd5b6fbab1438b88
William Bettridge-Radford <william.bettridge.radford@gail.com>
2021-12-18 16:49:23 +0000

    refactor printer

--- src/py/parse.py
+++ src/py/parse.py
@@ -28,42 +28,49 @@ def is_atom(x):
     return not isinstance(x, list)
 
 
-def print_expr(x, depth=0, wrap=False):
+def print_expr(x, depth=0, print_brackets=True):
 
     if is_atom(x):
         print(x, end="")
         return
 
-    print("[", end="")
+    if print_brackets:
+        print("[", end="")
 
     car, *cdr = x
 
-    print_expr(car, depth+1)
+    if not is_atom(car):
+        if not print_brackets:
+            print("\\\\", end="")
+        print("\n" + (depth+1) * "    ", end="")
+
+    print_expr(car, depth+1, print_brackets)
 
     prev_it = car
 
     for it in cdr:
-        if is_atom(it):
-            if is_atom(prev_it):
-                print(" ", end="")
-            else:
-                print("\n" + (depth+1) * "    ", end="")
-            print_expr(it, depth+1)
+        if is_atom(it) and is_atom(prev_it):
+            print(" ", end="")
         else:
             print("\n" + (depth+1) * "    ", end="")
-            print_expr(it, depth+1)
+
+        print_expr(it, depth+1, print_brackets)
+
         prev_it = it
 
-    print("]", end="")
+    if print_brackets:
+        print("]", end="")
+
+
+def puts_expr(x, print_brackets=True):
+    print_expr(x, 0, print_brackets)
+    print("")
 
 
 if __name__ == '__main__':
     prog = parse_file('-')
     # pprint(prog)
+    puts_expr(prog, False)
     print()
-    for x in prog:
-        # print('x:', x)
-        print_expr(x)
-        print("\n")
-    print()
+
 
