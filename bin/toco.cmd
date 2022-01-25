cfad9d1d0100310f74bb6f2151bb629ecb3bc3f9
William Bettridge-Radford <william.bettridge.radford@gail.com>
2022-01-25 08:31:50 +0000

    launch scripts for windows

---
+++ bin/toco.cmd
@@ -0,0 +1,9 @@
+@echo off
+set here=%~dp0
+set py_dir=%here%..\src\py\
+rem echo %here%
+rem echo %py_dir%
+python %py_dir%tokenise.py %1 | ^
+python %py_dir%parse1_indent.py - | ^
+python %py_dir%toco.py -
+
